8b9e7bc39e987a5ab4bd2dbee2b1fd1af5488d6a
jyoti ade <jyotiade287@gmail.com>
2024-09-09 18:41:43 +0530

    cookies

---
+++ project/app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('set/',set,name="set"),
+    path('get/',get,name="get")
+]
---
+++ project/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def set(request):
+    data=render(request,'set.html')
+    data.set_cookie('name','jyoti',max_age=5*24*60*60,httponly=True,secure=True)  #max_age used to set maximum time for cookies data
+    data.set_cookie('age','21',max_age=4*24*60*60,httponly=True,secure=True)   #httponly used to only open it with https
+    data.set_cookie('city','bhopal',max_age=3*24*60*60,httponly=True,secure=True)
+    return data
+
+def get(request):
+    print(request.COOKIES)
+    name=request.COOKIES['name']
+    age=request.COOKIES['age']
+    city=request.COOKIES['city']
+
+    data={
+        'name':name,
+        'age':age,
+        'city':city
+    }
+    return render(request,'get.html',data)
